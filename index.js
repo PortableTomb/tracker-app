05273aefe62b2c0e902710c1ed973684d2d2744d
simplesthing <i.am.simplesthing@gmail.com>
2017-01-18 16:02:42 -0800

    install koa

--- index.js
+++ index.js
@@ -1,2 +1,28 @@
+var koa = require('koa');
+var app = koa();
 
-// application entrypoint
+// x-response-time
+
+app.use(function *(next){
+  var start = new Date();
+  yield next;
+  var ms = new Date() - start;
+  this.set('X-Response-Time', ms + 'ms');
+});
+
+// logger
+
+app.use(function *(next){
+  var start = new Date();
+  yield next;
+  var ms = new Date() - start;
+  console.log('%s %s - %s', this.method, this.url, start);
+});
+
+// response
+
+app.use(function *(){
+  this.body = 'Hello World';
+});
+
+app.listen(3000);
